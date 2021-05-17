a0d381f087cd6f6d420e36a4b7d3464d3b68b54f
sajal <sajalv128@gmail.com>
2021-05-17 16:24:40 +0200

    Added semantic versioning 1.0.0.{build_run_no}, added powershell to updated version on every build run on build agent

---
+++ VersionUpdater.ps1
@@ -0,0 +1,12 @@
+﻿$version = $args[0]
+
+$manifestFile = Resolve-Path $PSScriptRoot\GitBranchDiffer\source.extension.vsixmanifest
+
+[xml]$manifestFileContent = Get-Content $manifestFile
+
+$manifestFileContent.PackageManifest.Metadata.Identity.Version = $version
+
+$manifestFileContent.Save($manifestFile)
+
+Write-Host "Updated VSIX version to "$version
+
