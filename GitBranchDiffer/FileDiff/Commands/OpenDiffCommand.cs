7c00fc0678b82447621f4ab5ec1295b3dc41aa80
sajal <sajalv128@gmail.com>
2021-06-09 22:59:07 +0200

    Remove custom UI context to control visibility, instead, buttons visiblity are bound to Commands, so toggle command visiblity

---
+++ GitBranchDiffer/FileDiff/Commands/OpenDiffCommand.cs
@@ -0,0 +1,14 @@
+﻿using GitBranchDiffer.SolutionSelectionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchDiffer.FileDiff.Commands
+{
+    public abstract class OpenDiffCommand
+    {
+        public abstract ISolutionSelection GetSolutionSelection();
+    }
+}
