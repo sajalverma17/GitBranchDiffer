dc7c54381276598c300062143ccbae1e59c68b23
sajal <sajalv128@gmail.com>
2021-07-17 14:18:07 +0200

    Moved build scripts to build, files needed to deploy to deploy folder

--- VersionParser.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$tagName = git describe --tag --abbrev=0
-
-# Tag pattern is v*.*.*
-$versionString = $tagname.Substring(1, $tagname.length - 1)
-
-Write-Host "Version number from latest Git tag: "$versionString
-
-return $versionString
