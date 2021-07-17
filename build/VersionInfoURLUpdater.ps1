1e3056521ecba2c80342673b951211081dfb095a
sajal <sajalv128@gmail.com>
2021-07-17 14:21:22 +0200

    Commit build folder by remove it from gitignore

---
+++ build/VersionInfoURLUpdater.ps1
@@ -0,0 +1,12 @@
+﻿$releaseUrl = $args[0]
+
+$manifestFile = Resolve-Path $PSScriptRoot\GitBranchDiffer\source.extension.vsixmanifest
+
+[xml]$manifestFileContent = Get-Content $manifestFile
+
+$manifestFileContent.PackageManifest.Metadata.ReleaseNotes = $releaseUrl
+
+$manifestFileContent.Save($manifestFile)
+
+Write-Host "Updated ReleaseNotes URL in VSIX manifest to "$releaseUrl
+
---
+++ build/VersionUpdater.ps1
@@ -0,0 +1,12 @@
+﻿$version = $args[0]
+
+$manifestFile = Resolve-Path $PSScriptRoot\GitBranchDiffer\source.extension.vsixmanifest
+
+[xml]$manifestFileContent = Get-Content $manifestFile
+
+$manifestFileContent.PackageManifest.Metadata.Identity.Version = $version
+
+$manifestFileContent.Save($manifestFile)
+
+Write-Host "Updated version in VSIX Manifest to "$version
+
