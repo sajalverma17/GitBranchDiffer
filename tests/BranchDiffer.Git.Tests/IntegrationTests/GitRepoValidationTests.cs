0bd692b89e9d76e2e2ca11692d9cf5c3aa01b31b
sajal <sajalv128@gmail.com>
2021-10-26 12:23:39 +0200

    Added tests

---
+++ tests/BranchDiffer.Git.Tests/IntegrationTests/GitRepoValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using BranchDiffer.Git.Configuration;
+using BranchDiffer.Git.Core;
+using BranchDiffer.Git.Exceptions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace BranchDiffer.Git.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class GitRepoValidationTests : IntegrationTestBase
+    {
+        [Test]
+        public void GenerateDiff_MustThrowException_When_BranchToDiffAgainst_Null()
+        {
+            // ARRANGE
+            var controllerUnderTest = DIContainer.Instance.GetService(typeof(GitBranchDiffController)) as GitBranchDiffController;
+
+            // ACT + ASSERT Should throw a git operation exception
+            Assert.Throws<GitOperationException>(() => controllerUnderTest.GenerateDiff(this.TestGitRepoPath, null));
+        }
+
+        [Test]
+        public void GenerateDiff_MustThrowException_When_BranchToDiffAgainst_NotFound()
+        {
+            // ARRANGE
+            var controllerUnderTest = DIContainer.Instance.GetService(typeof(GitBranchDiffController)) as GitBranchDiffController;
+
+            // ACT + ASSERT Should throw a git operation exception
+            Assert.Throws<GitOperationException>(() => controllerUnderTest.GenerateDiff(this.TestGitRepoPath, "Bob Loblaw"));
+        }
+
+        [Test]
+        public void GenerateDiff_MustThrowException_When_BranchToDiffAgainst_Same_As_Head()
+        {
+            // ARRANGE
+            var controllerUnderTest = DIContainer.Instance.GetService(typeof(GitBranchDiffController)) as GitBranchDiffController;
+
+            // ACT + ASSERT Should throw a git operation exception
+            Assert.Throws<GitOperationException>(() => controllerUnderTest.GenerateDiff(this.TestGitRepoPath, this.TestRepoHEAD));
+        }
+    }
+}
--- tests/BranchDiffer.Git.Tests/IntegrationTests/IntegrationTestBase.cs
+++ tests/BranchDiffer.Git.Tests/IntegrationTests/IntegrationTestBase.cs
@@ -17,13 +17,18 @@ namespace BranchDiffer.Git.Tests.IntegrationTests
             // Path to test VS solution
             this.TestSolutionPath = @"..\Assets\GitBranchDiffer-TestAsset\ProjectUnderTest.sln";
 
-            // Path to Git repo
+            // Path to test Git repo
             this.TestGitRepoPath = TestContext.CurrentContext.TestDirectory + @"\..\..\..\Assets\GitBranchDiffer-TestAsset\";
+
+            // The name of HEAD to which the test Git Repo points to
+            this.TestRepoHEAD = "feature/test-feature";
         }
 
         protected string TestSolutionPath { get; private set; }
 
         protected string TestGitRepoPath { get; private set; }
+
+        protected string TestRepoHEAD { get; private set; } 
 
     }
 }
