39de04dd47389d0d42021e646e25d7acc668e1ee
sajalverma17 <sajalv128@gmail.com>
2023-01-01 19:33:06 +0100

    Add a build script for ease of locally testing builds

---
+++ build/build.ps1
@@ -0,0 +1,28 @@
+﻿$buildConfig = $args[0]
+
+function build {
+	param ([string]$config)
+
+If($config -eq "Debug" -or $config -eq "Release") {
+
+    Write-Host "Restoring and building with build config $config"
+
+    msbuild ..\src\GitBranchDiffer.sln /t:Restore /p:Configuration=$config /v:m
+
+    msbuild ..\src\GitBranchDiffer.sln /p:Configuration=$config /p:Platform="Any CPU" /v:m
+
+    return
+}
+
+    Write-Host "Invalid build configuration specified"
+
+    return
+}
+
+try {
+    build($buildConfig)
+}
+catch{
+    Write-Host $_.ScriptStackTrace
+    Exit 1
+}
