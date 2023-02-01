eff706e66535c5517bfde8bf404bf5ef1b15a049
sajalverma17 <sajalv128@gmail.com>
2023-02-01 16:52:47 +0100

    Fix #41 by checking if AutoLoad of VsPackage happens due to a dummy Solution that VS creates when it opens document separate from a solution

--- src/BranchDiffer.VS.Shared/GitBranchDifferPackage.cs
+++ src/BranchDiffer.VS.Shared/GitBranchDifferPackage.cs
@@ -21,7 +21,7 @@ namespace BranchDiffer.VS.Shared
     [Guid(GitBranchDifferPackageGuids.guidBranchDiffWindowPackage)]
     [ProvideOptionPage(typeof(GitBranchDifferPluginOptions),
     "Git Branch Differ", "Git Branch Differ Options", 0, 0, true)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class GitBranchDifferPackage : AsyncPackage, IGitBranchDifferPackage
     {
         private EnvDTE.DTE dte;
@@ -38,30 +38,35 @@ namespace BranchDiffer.VS.Shared
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             this.dte = await GetServiceAsync(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
-            IVsUIShell uiShell = await GetServiceAsync(typeof(SVsUIShell)) as IVsUIShell;
 
-            // Construct file diff commands, initialize dependecies in it and then register them in VS Menu Commands asynchronously
-            this.openPhysicalFileDiffCommand = VsDIContainer.Instance.GetService(typeof(OpenPhysicalFileDiffCommand)) as OpenPhysicalFileDiffCommand;
-            this.openProjectFileDiffCommand = VsDIContainer.Instance.GetService(typeof(OpenProjectFileDiffCommand)) as OpenProjectFileDiffCommand;
-            await this.openPhysicalFileDiffCommand.InitializeAndRegisterAsync(this);
-            await this.openProjectFileDiffCommand.InitializeAndRegisterAsync(this);
+            await this.RegisterCommandsAsync();
 
             if (dte != null)
             {
-                // Filter will be initialized on package load.
-                // Also hook to all relevant events here so we can solution-info set on the Filter when they are fired
-                this.dte.Events.SolutionEvents.Opened += SetSolutionInfo;
-                this.dte.Events.SolutionEvents.BeforeClosing += ResetSolutionInfo;
-                this.SetSolutionInfo();
+                // Hook on to events to set/reset solution path on BranchDiffFilter
+                this.dte.Events.SolutionEvents.Opened += SetSolutionPathOnFilter;
+                this.dte.Events.SolutionEvents.BeforeClosing += ClearSolutionPathFromFilter;
+
+                // When a document is opened separate from a solution, VS loads a "dummy" Solution1.
+                // This leads to our extension initialized (due to ProvideAutoLoad).
+                // In this case, don't set solution path now,
+                // let it happen when SolutionEvents.Opened triggers it on some "real" solution, if opens one later.
+                if (!string.IsNullOrEmpty(this.dte.Solution.FullName))
+                {
+                    this.SetSolutionPathOnFilter();
+                }
+                
             }
             else
             {
+                IVsUIShell uiShell = await GetServiceAsync(typeof(SVsUIShell)) as IVsUIShell;
+
                 Guid clsid = Guid.Empty;
                 uiShell.ShowMessageBox(
                     0,
                     ref clsid,
                     "FirstPackage",
-                    "Unable to load Git Branch Differ plug -in.Failed to get Visual Studio services",
+                    "Unable to load Git Branch Differ plug-in. Failed to get Visual Studio services.",
                     string.Empty,
                     0,
                     OLEMSGBUTTON.OLEMSGBUTTON_OK,
@@ -86,10 +91,17 @@ namespace BranchDiffer.VS.Shared
 
         public CancellationToken CancellationToken => this.DisposalToken;
 
-        /// <summary>
-        /// Sets the Solution directory and name info on Branch Diff Filter.
-        /// </summary> 
-        private void SetSolutionInfo()
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "VSSDK006:Check services exist", Justification = "Show custom error if DTE service doesn't exist")]
+        private async Task RegisterCommandsAsync()
+        {
+            // Construct file diff commands, initialize dependecies in it and then register them in VS Menu Commands asynchronously
+            this.openPhysicalFileDiffCommand = VsDIContainer.Instance.GetService(typeof(OpenPhysicalFileDiffCommand)) as OpenPhysicalFileDiffCommand;
+            this.openProjectFileDiffCommand = VsDIContainer.Instance.GetService(typeof(OpenProjectFileDiffCommand)) as OpenProjectFileDiffCommand;
+            await this.openPhysicalFileDiffCommand.InitializeAndRegisterAsync(this);
+            await this.openProjectFileDiffCommand.InitializeAndRegisterAsync(this);
+        }
+
+        private void SetSolutionPathOnFilter()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
             var absoluteSolutionPath = this.dte.Solution.FullName;
@@ -97,10 +109,7 @@ namespace BranchDiffer.VS.Shared
             BranchDiffFilterProvider.SetSolutionInfo(solutionDirectory);
         }
 
-        /// <summary>
-        /// Resets the filter by removing the solution info (set to string.Empty)
-        /// </summary>
-        private void ResetSolutionInfo()
+        private void ClearSolutionPathFromFilter()
         {
             BranchDiffFilterProvider.SetSolutionInfo(string.Empty);
         }
