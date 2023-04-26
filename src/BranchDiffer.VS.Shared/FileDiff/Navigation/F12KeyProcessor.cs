f6cc38b5c5b509ffbff8d923c4c2995973405f30
sajalverma17 <sajalv128@gmail.com>
2023-04-26 10:01:39 +0200

    Try out keyprocessor to GotToDef F12 behaviour, but this likely won't override vs default behaviour

---
+++ src/BranchDiffer.VS.Shared/FileDiff/Navigation/F12KeyProcessor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BranchDiffer.VS.Shared.FileDiff.Navigation
+{
+    // Listen for F12 being pressed
+    
+}
---
+++ src/BranchDiffer.VS.Shared/FileDiff/Navigation/F12KeyProcessorProvider.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using static Microsoft.VisualStudio.Shell.RegistrationAttribute;
+using System.Windows.Forms;
+using System.Windows.Input;
+using KeyEventHandler = System.Windows.Input.KeyEventHandler;
+using Key = System.Windows.Input.Key;
+
+namespace BranchDiffer.VS.Shared.FileDiff.Navigation
+{
+    [Export(typeof(IKeyProcessorProvider))]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TextViewRole(PredefinedTextViewRoles.EmbeddedPeekTextView)]
+    [ContentType("code")]
+    [Name("GitBranchDifferNavigation")]
+    [Order(Before = "VisualStudioKeyboardProcessor")]
+    public class F12KeyProcessorProvider : IKeyProcessorProvider
+    {
+        public KeyProcessor GetAssociatedProcessor(IWpfTextView wpfTextView)
+        {
+            return new F12KeyProcessor(wpfTextView);
+        }
+    }
+
+    internal class F12KeyProcessor : KeyProcessor
+    {
+        private readonly ITextView _textView;
+
+        public F12KeyProcessor(ITextView textView)
+        {
+            _textView = textView;
+        }
+
+        public override void KeyDown(System.Windows.Input.KeyEventArgs args)
+        {
+            if (IsF12)
+            {
+                if (_textView.Caret != null)
+                {                    
+                }
+            }
+        }
+
+        public bool IsF12
+        {
+            get { return Keyboard.IsKeyDown(Key.F12) || Keyboard.IsKeyDown(Key.F12); }
+        }
+    }
+}
